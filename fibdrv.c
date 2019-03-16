bdd47fa3a76280fe1cecbca807d00515012dcf19
Ting-Fu Liao <tingfuliao@gmail.com>
2019-03-17 00:24:39 +0800

    Create sysfs for statistic information
        - /sys/kernel/fibonacci/{reset,result}

--- fibdrv.c
+++ fibdrv.c
@@ -9,6 +9,12 @@
 #include <linux/module.h>
 #include <linux/mutex.h>
 
+#include <linux/kobject.h>
+#include <linux/slab.h>
+#include <linux/sysfs.h>
+
+#define ENABLE_STAT
+
 MODULE_LICENSE("Dual MIT/GPL");
 MODULE_AUTHOR("National Cheng Kung University, Taiwan");
 MODULE_DESCRIPTION("Fibonacci engine driver");
@@ -25,6 +31,75 @@ static dev_t fib_dev = 0;
 static struct cdev *fib_cdev;
 static struct class *fib_class;
 static DEFINE_MUTEX(fib_mutex);
+
+#ifdef ENABLE_STAT
+static struct kobject *stat_kobj;
+static uint64_t *stat_ns = NULL;
+static uint32_t *stat_cnt = NULL;
+#endif /* ENABLE_STAT */
+
+#ifdef ENABLE_STAT
+static ssize_t result_show(struct kobject *kobj,
+                           struct kobj_attribute *attr,
+                           char *buf)
+{
+    size_t limit = PAGE_SIZE - 32;
+    size_t len = 0;
+    int i;
+    for (i = 0; i <= MAX_LENGTH && len < limit; ++i) {
+        if (stat_cnt[i] == 0) {
+            continue;
+        }
+        len += snprintf(buf + len, limit - len, "%d: %llu / %lu\n", i,
+                        (unsigned long long) stat_ns[i],
+                        (unsigned long) stat_cnt[i]);
+    }
+    if (len >= limit) {
+        len +=
+            snprintf(buf + len, PAGE_SIZE - len, "... more lines truncated\n");
+    }
+    return len;
+}
+
+static struct kobj_attribute result_attribute =
+    __ATTR(result, 0444, result_show, NULL);
+
+static ssize_t reset_show(struct kobject *kobj,
+                          struct kobj_attribute *attr,
+                          char *buf)
+{
+    return sprintf(buf, "store 1 to trigger stat data reset\n");
+}
+
+static ssize_t reset_store(struct kobject *kobj,
+                           struct kobj_attribute *attr,
+                           const char *buf,
+                           size_t count)
+{
+    int ret, val, i;
+    ret = kstrtoint(buf, 10, &val);
+    if (ret == 0) {
+        for (i = 0; i <= MAX_LENGTH; ++i) {
+            stat_ns[i] = 0;
+            stat_cnt[i] = 0;
+        }
+    }
+    return count;
+}
+
+static struct kobj_attribute reset_attribute =
+    __ATTR(reset, 0644, reset_show, reset_store);
+
+static struct attribute *attrs[] = {
+    &result_attribute.attr,
+    &reset_attribute.attr,
+    NULL, /* need to NULL terminate the list of attributes */
+};
+
+static struct attribute_group stat_group = {
+    .attrs = attrs,
+};
+#endif /* ENABLE_STAT */
 
 static long long fib_sequence(long long k)
 {
@@ -63,11 +138,15 @@ static ssize_t fib_read(struct file *file,
                         loff_t *offset)
 {
     ssize_t r;
+#ifdef ENABLE_STAT
     ktime_t begin = ktime_get();
+#endif /* ENABLE_STAT */
     r = fib_sequence(*offset);
-    struct timespec64 ts = ktime_to_timespec64(ktime_get() - begin);
-    printk(KERN_INFO "fib_sequence(%lld) takes %lld.%09ld seconds\n",
-           (long long) *offset, (long long) ts.tv_sec, (long) ts.tv_nsec);
+#ifdef ENABLE_STAT
+    uint64_t ns = ktime_to_ns(ktime_get() - begin);
+    stat_ns[*offset] += ns;
+    stat_cnt[*offset]++;
+#endif /* ENABLE_STAT */
     return r;
 }
 
@@ -111,6 +190,34 @@ const struct file_operations fib_fops = {
     .release = fib_release,
     .llseek = fib_device_lseek,
 };
+
+#ifdef ENABLE_STAT
+static int init_stat(void)
+{
+    stat_kobj = kobject_create_and_add("fibonacci", kernel_kobj);
+    if (!stat_kobj) {
+        printk(KERN_ERR "Failed to create sysfs object");
+        return -ENOMEM;
+    }
+
+    int rc = sysfs_create_group(stat_kobj, &stat_group);
+    if (rc) {
+        kobject_put(stat_kobj);
+    }
+
+    stat_ns = kcalloc(MAX_LENGTH + 1, sizeof(uint64_t), GFP_KERNEL);
+    stat_cnt = kcalloc(MAX_LENGTH + 1, sizeof(uint32_t), GFP_KERNEL);
+
+    if (!stat_ns || !stat_ns) {
+        kfree(stat_cnt);
+        kfree(stat_ns);
+        kobject_put(stat_kobj);
+        return -ENOMEM;
+    }
+
+    return rc;
+}
+#endif /* ENABLE_STAT */
 
 static int __init init_fib_dev(void)
 {
@@ -157,7 +264,17 @@ static int __init init_fib_dev(void)
         rc = -4;
         goto failed_device_create;
     }
+#ifdef ENABLE_STAT
+    rc = init_stat();
+    if (rc < 0) {
+        goto failed_stat_create;
+    }
+#endif /* ENABLE_STAT */
     return rc;
+#ifdef ENABLE_STAT
+failed_stat_create:
+    device_destroy(fib_class, fib_dev);
+#endif /* ENABLE_STAT */
 failed_device_create:
     class_destroy(fib_class);
 failed_class_create:
@@ -170,6 +287,11 @@ failed_cdev:
 static void __exit exit_fib_dev(void)
 {
     mutex_destroy(&fib_mutex);
+#ifdef ENABLE_STAT
+    kfree(stat_cnt);
+    kfree(stat_ns);
+    kobject_put(stat_kobj);
+#endif /* ENABLE_STAT */
     device_destroy(fib_class, fib_dev);
     class_destroy(fib_class);
     cdev_del(fib_cdev);
